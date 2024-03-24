7611b88aba78e8dfffb718f4e81fa016f2a3bd1b
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2024-03-24 15:37:12 +0800

    Update typed.js

--- typed.js
+++ typed.js
@@ -1,7 +1,7 @@
 var typed4 = new Typed("#names", {
   strings: [
     "Asai, Yaya, Ahmad, Laila, dll",
-    "Asai (221001xxx), Yaya (221002xxx), Ahmad,  Ara (221003xxx)",
+    "Asai (221001xxx), Yaya (221002xxx), Ahmad, Ara (221003xxx)",
   ],
   typeSpeed: 30,
   backSpeed: 30,
