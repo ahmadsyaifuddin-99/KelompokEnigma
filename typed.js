df8ced75a51df0781372fe2fc4b5ba6675c14764
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2024-03-24 15:24:52 +0800

    add typed js

---
+++ typed.js
@@ -0,0 +1,8 @@
+var typed4 = new Typed("#names", {
+  strings: ["Some strings without", "Some HTML", "Chars"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  attr: "placeholder",
+  bindInputFocusEvents: true,
+  loop: true,
+});
