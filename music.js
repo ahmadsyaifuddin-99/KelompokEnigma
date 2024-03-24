c17df425eefb313341a654035f6f4acf89cb1963
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2024-03-24 18:01:22 +0800

    menambahkan fitur music

---
+++ music.js
@@ -0,0 +1,40 @@
+// Tambahkan variabel global untuk status audio
+var audioStatus = true;
+
+// Fungsi untuk mengaktifkan/menonaktifkan audio
+function toggleAudio() {
+  var audio = document.getElementById("audio");
+  var audioButton = document.getElementById("audioButton");
+  if (audio.paused) {
+    audio.play();
+    audioStatus = true;
+    audioButton.innerHTML = '<i class="fas fa-volume-mute"></i>'; // Mengubah ikon menjadi mute
+  } else {
+    audio.pause();
+    audioStatus = false;
+    audioButton.innerHTML = '<i class="fas fa-music fa-lg"></i>'; // Mengubah ikon menjadi music
+  }
+}
+
+// Tambahkan audio di sini
+var audio = new Audio("./asset/fireflies.mp3");
+audio.setAttribute("id", "audio");
+document.body.appendChild(audio);
+
+// Gaya untuk widget audio
+var audioWidget = document.getElementById("audioWidget");
+audioWidget.style.position = "fixed";
+audioWidget.style.bottom = "20px"; // Ubah ke 20px atau sesuai kebutuhan
+audioWidget.style.right = "20px";
+audioWidget.style.zIndex = "999";
+
+// Panggil fungsi scroll
+window.addEventListener("scroll", function () {
+  // Ubah posisi widget audio saat digulir
+  audioWidget.style.bottom = "20px"; // Tetapkan nilai tetap
+});
+
+// Jalankan fungsi toggleAudio() saat konten dimuat
+window.addEventListener("DOMContentLoaded", function () {
+  toggleAudio(); // Memastikan audio diputar otomatis saat halaman dimuat
+});
