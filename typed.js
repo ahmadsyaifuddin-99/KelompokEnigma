4283573dd08605df17c17d6d66b3e401f14806d7
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2024-03-24 15:37:00 +0800

    ok typed js var added

--- typed.js
+++ typed.js
@@ -1,8 +1,12 @@
 var typed4 = new Typed("#names", {
-  strings: ["Some strings without", "Some HTML", "Chars"],
-  typeSpeed: 50,
-  backSpeed: 50,
+  strings: [
+    "Asai, Yaya, Ahmad, Laila, dll",
+    "Asai (221001xxx), Yaya (221002xxx), Ahmad,  Ara (221003xxx)",
+  ],
+  typeSpeed: 30,
+  backSpeed: 30,
   attr: "placeholder",
+  cursorChar: "",
   bindInputFocusEvents: true,
   loop: true,
 });
