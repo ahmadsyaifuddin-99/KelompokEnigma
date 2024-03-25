a2950b173aa29ce2a736777b7a7b5e33368803b0
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-99@users.noreply.github.com>
2024-03-25 09:35:09 +0800

    Update typed.js

--- js/typed.js
+++ js/typed.js
@@ -4,7 +4,7 @@ var typed4 = new Typed("#names", {
     "Asai (221001xxx), Yaya (221002xxx), Ahmad, Ara (221003xxx)",
   ],
   typeSpeed: 75,
-  backSpeed: 75,
+  backSpeed: 15,
   attr: "placeholder",
   cursorChar: "",
   bindInputFocusEvents: true,
